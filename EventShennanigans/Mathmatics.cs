a7d891a2dfd443847b196b54147de0c7e603e148
GillesBonte <gilles.bonte1@telenet.be>
2023-10-09 17:10:22 +0200

    Add project files.

---
+++ EventShennanigans/Mathmatics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+/// BEGIN HERE
+/// I made a class Mathmatics with an EventHandler and an Action,
+/// just to show the functionality and implementation of each one
+///
+/// follow this class to see how we make and invoke both events,
+/// then jump to Program to see how it can be implemented and executed by devs.
+
+
+namespace EventShennanigans
+{
+    public class Mathmatics
+    {
+        //definition of Event
+        public event EventHandler DivisibleBy3;
+
+        //definition of Action with an integer (not required)
+        public Action<int> DivisibleBy5;
+
+        //may aswell be:
+        //public Action DivisibleBy5;
+
+        public int NumberOne {  get; set; }
+        public int NumberTwo { get; set; }
+
+        public int CalculateSum()
+        {
+
+            int intResult = 0;
+            intResult = NumberOne + NumberTwo;
+           //check to invoke the Event
+            if (intResult%3 == 0 )
+            {
+                //With an Eventhandler we need to use parameters (object, Eventargs)
+                DivisibleBy3?.Invoke(this, new EventArgs());
+            }
+            //check to invoke the Action
+            if (intResult%5 == 0)
+            {
+                //With an Action we only need to add parameters of what we have put between <>
+                //in this example: Action<int> and we give the result to the mainprogram
+                DivisibleBy5?.Invoke(intResult);
+                //u may aswell have used Action<int, string, DateTime> or Action<string, int> etc...
+            }
+
+            return intResult;
+
+        }
+
+    }
+
+}
---
+++ EventShennanigans/Program.cs
@@ -0,0 +1,45 @@
+﻿using EventShennanigans;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+       /// Start in the class Mathmatics to see the creation and invocation of the events
+       /// 
+       /// This Program triggers the events and adds implementation that needs to happen
+       /// when one is triggered. <summary>
+
+    private static void Main(string[] args)
+    {
+        //define an instance of our mathmatics class
+        Mathmatics _maths = new Mathmatics();
+      
+        //we add a method for what needs to happen when each of the events is triggered
+        _maths.DivisibleBy3 += Triggered;
+        _maths.DivisibleBy5 += Triggered2;
+       
+        //put some testdata in the instance
+        _maths.NumberOne = 6;
+        _maths.NumberTwo = 9;
+
+        //execute the method we made to invoke the events
+       _maths.CalculateSum();
+
+    }
+
+
+    //When the event is invoked, execute this code
+    // notice the empty/unused object, Eventargs parameters
+    private static void Triggered(object? sender, EventArgs e)
+    {
+        Console.WriteLine("The result is divisible by 3");
+    }
+
+    //When the action is invoked, execute this code
+    // notice the output we get back from our Action<Int> and are able to use.
+    private static void Triggered2(int output)
+    {
+        Console.WriteLine("The result is divisible by 5");
+        Console.WriteLine($"the output is: {output}.");
+    }
+
+}
